1ef427881c6ecbaec9198f10eac06dead3518fb0
Jens Goossens <jensgoossens@telenet.be>
2016-10-19 21:29:38 +0200

    bump version, change dependencies and start over :)

--- src/test/java/com/connect_group/thymeleaf_extras/config/TestConfig.java
+++
@@ -1,9 +0,0 @@
-package com.connect_group.thymeleaf_extras.config;
-
-import com.connect_group.thymeleaf.testing.config.ExtendableTestSpringContext;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
-@Configuration
-@Import({ThymesheetExtrasConfig.class})
-public class TestConfig extends ExtendableTestSpringContext {}
